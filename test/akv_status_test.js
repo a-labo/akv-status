95d60c6f71d604660d652266f31c35a3e6c4aa93
okunishinishi <okunishinishi@gmail.com>
2017-09-11 10:57:36 +0900

    Update deps

--- test/akv_status_test.js
+++ test/akv_status_test.js
@@ -14,36 +14,36 @@ const path = require('path')
 describe('akv-status', function () {
   this.timeout(3000)
 
-  before(() => co(function * () {
+  before(async () => {
 
-  }))
+  })
 
-  after(() => co(function * () {
+  after(async () => {
 
-  }))
+  })
 
-  it('Akv status', () => co(function * () {
-    let tmplDir = `${__dirname}/../tmp/testing-status-file`
+  it('Akv status', async () => {
+    const tmplDir = `${__dirname}/../tmp/testing-status-file`
     mkdirp.sync(tmplDir)
-    let [filename01, filename02] = [
+    const [filename01, filename02] = [
       path.resolve(tmplDir, 'hoge01.txt'),
       path.resolve(tmplDir, 'hoge02.txt')
     ]
     fs.writeFileSync(filename01, '01-' + new Date())
     fs.writeFileSync(filename02, '02-' + new Date())
     let store = new AkvStatus(`${tmplDir}/testing.status.json`)
-    yield store.saveStatus([ filename01, filename02 ])
+    await store.saveStatus([filename01, filename02])
     fs.writeFileSync(filename01, '01-updated-' + new Date())
     {
-      let filtered = yield store.filterStatusUnknown([ filename01, filename02 ])
-      assert.deepEqual(filtered, [ filename01 ])
+      let filtered = await store.filterStatusUnknown([filename01, filename02])
+      assert.deepEqual(filtered, [filename01])
     }
     fs.writeFileSync(filename02, '02-updated-' + new Date())
     {
-      let filtered = yield store.filterStatusUnknown([ filename01, filename02 ])
-      assert.deepEqual(filtered, [ filename01, filename02 ])
+      let filtered = await store.filterStatusUnknown([filename01, filename02])
+      assert.deepEqual(filtered, [filename01, filename02])
     }
-  }))
+  })
 })
 
 /* global describe, before, after, it */
